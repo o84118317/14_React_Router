e52f98491a14db19a3baebab465b9f3c9a321ca4
o84118317 <o84118317@gmail.com>
2018-10-15 17:20:19 +0800

    redirect and no match

---
+++ 07_No_match.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Switch
+	Redirect
+} from 'react-router-dom'
+
+
+const Home = () => <h1>Home</h1>
+const WillMatch = () => <h1>Matched!<h1>
+
+const NoMatch = ({ location }) => (
+	<div>
+		<h3>No match for <code>{location.pathname}</code></h3>
+	</div>
+)
+
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div>
+					<ul>
+						<li><Link to='/'>Home</Link></li>
+						<li><Link to='/old-match'>Old Match, to be redirected</Link></li>
+						<li><Link to='/will-match'>Will Match</Link></li>
+						<li><Link to='/will-not-match'>Will Not Match</Link></li>
+						<li><Link to='/also/will-not-match'> Also Will Not Match</Link></li>
+					</ul>
+
+
+
+					<Switch>
+						<Route path='/' exact component={Home} />
+						<Redirect from='/old-match' to='will-match' />
+						<Route path='/will-match' component={WillMatch} />
+						<Route component={NoMatch} />
+					</Switch>
+
+				</div>
+			</Router>
+
+		)
+	}
+}
+
+export defautl App
