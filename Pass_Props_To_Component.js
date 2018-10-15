797f2564fb27c008a899aa7bb9573989399471c5
o84118317 <o84118317@gmail.com>
2018-10-15 15:53:45 +0800

    pass props to a component rendered by Reacr Router

--- Pass_Props_To_Component.js
+++ Pass_Props_To_Component.js
@@ -20,3 +20,29 @@ const routes = [
 		fetchInitialData: (id) => fetchDashboard(id),
 	}
 ]
+
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div>
+					<ul>
+						<li><Link to='/settings'>Settings</Link></li>
+						<li><Link to='/dashboard'>Dashboard</Link></li>
+					</ul>
+
+						{routes.map(({ path, component: C, fetchInitialData}) => (
+							<Route
+								path={path}
+								render={(props) => <C {...props} fetchInitialData={fetch} />}
+							/>
+						))}
+					<hr />
+
+
+				</div>
+			</Router>
+		)
+	}
+}
