e4c84304a230dd5b1bb8a163889c329fe2e16bb7
o84118317 <o84118317@gmail.com>
2018-10-15 17:36:16 +0800

    Ambiguous Matches

---
+++ 08_Ambiguous_Matches.js
@@ -0,0 +1,42 @@
+import React { Component } from 'react'
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Switch
+} from 'react-router-dom'
+
+
+const About = () => <h2>About</h2>
+const Company = () => <h2>Company</h2>
+const User = ( {match} ) => (
+	<div>
+		<h2>User: {match.params.user}</h2>
+	</div>
+)
+
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div>
+
+					<ul>
+						<li><Link to='/about'>About</Link></li>
+						<li><Link to='/company'>Company</Link></li>
+						<li><Link to='/kim'>Kim</Link></li>
+						<li><Link to='/chris'>Chris</Link></li>
+					</ul>
+					<Switch>
+						<Route path='/about' component={About} />
+						<Route path='/company' component={Company} />
+						<Route path='/:user' component={User} />
+					</Switch>
+				</div>
+			</Router>
+		)
+	}
+}
+
+export default App
