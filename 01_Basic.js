09902e900ea0bcba211568574b0894c5ceba9958
o84118317 <o84118317@gmail.com>
2018-10-22 18:30:41 +0800

    modified

--- 01_Basic.js
+++ 01_Basic.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const BasicExample = () => {
-  	<Router>
+	<Router>
 		<div>
 			<ul>
 				<li>
